f761a84f3aca61308a07b189e396ffe08fd8d2b4
christinab980 <71195516+christinab980@users.noreply.github.com>
2023-05-27 09:21:54 -0700

    first commit

---
+++ my-react-project/src/sectioning/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <div className="y-wrap">Header</div>
+    </header>
+  )
+};
+
+export default Header;
---
+++ my-react-project/src/sectioning/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <main>
+      <div className="y-wrap">Main</div>
+    </main>
+  )
+};
+
+export default Main;
