6450d373252dcdd653a084e92b8dfaf07e0c9d7f
christinab980 <71195516+christinab980@users.noreply.github.com>
2023-05-27 12:56:42 -0700

    changed the header to include the logo

--- my-react-project/src/sectioning/header.jsx
+++ my-react-project/src/sectioning/header.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import data from '../mocks/navs.json';
+import logo from '../mocks/home.json'
 
 const Header = () => {
-
+  const home = logo.map(nav => <NavLink key={nav.href} to={nav.href}><img className="logo" src={nav.src} /> </NavLink>)
   const navs = data.map(nav => <NavLink key={nav.href} to={nav.href}>{nav.name}</NavLink>);
 
   return (
     <header>
-      <div className="y-navs y-wrap">{navs}</div>
+      <div className="y-navs y-wrap">{home}{navs}</div>
     </header>
   );
 };
