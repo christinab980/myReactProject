f0816f12193a661e7c2d69b8eb30cebf8f442161
christinab980 <71195516+christinab980@users.noreply.github.com>
2023-05-27 15:59:18 -0700

    added api documentation information

---
+++ my-react-project/src/component/apiCall.jsx
@@ -0,0 +1,13 @@
+// API URLS with specific categories
+
+//https://quizapi.io/api/v1/questions?apiKey=YOUR_API_KEY&category=code&difficulty=Easy&limit=10&tags=HTML
+//https://quizapi.io/api/v1/questions?apiKey=YOUR_API_KEY&category=sql&limit=10&tags=MySQL
+//https://quizapi.io/api/v1/questions?apiKey=YOUR_API_KEY&category=code&difficulty=Easy&limit=10&tags=JavaScript
+
+
+//api documentation to start api call 
+
+// curl https://quizapi.io/api/v1/questions -G \
+// -d apiKey="w9Rvsy8CdKGevTtBTBwe0aGMiqhMO7sHiJx57y8Y" 
+// -d category=Programming
+// -d limit=10
