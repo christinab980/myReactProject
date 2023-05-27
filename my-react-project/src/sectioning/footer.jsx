f1c29841230c2e9c6bcbef62d084fd100f12930e
christinab980 <71195516+christinab980@users.noreply.github.com>
2023-05-27 09:56:03 -0700

    added github icon to footer and img

--- my-react-project/src/sectioning/footer.jsx
+++ my-react-project/src/sectioning/footer.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer>
-      <div className="y-wrap">Footer</div>
+      <div className='y-wrap'>
+      <a href="https://github.com/christinab980/myReactProject" target="_blank"> 
+      <img className='footer-icon' src="./src/img/github_icon.png" alt="gitHub icon" /> </a>
+      </div>
     </footer>
   )
 };
