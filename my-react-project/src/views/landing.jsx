039b00f942529e4cab46498d660857547aaa0d9f
christinab980 <71195516+christinab980@users.noreply.github.com>
2023-05-27 15:22:15 -0700

    added logo and landing page

--- my-react-project/src/views/landing.jsx
+++ my-react-project/src/views/landing.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 
 const Landing = () => {
   return (
-    <h1>Landing</h1>
+    <div className='landing-container'>
+      <img className='landing-quiz-logo'src="./src/img/quizLogo.png" alt="quiz-logo" />
+      <div className='landing-wrap'> 
+        <h1>Welcome to QuizLite!</h1>
+        <p> Test your knowledge on the latest programming languages! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+      </div>
+    </div>
   )
 };
 
