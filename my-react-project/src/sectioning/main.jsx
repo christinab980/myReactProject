f556d8798ccc54b4f8bd7b0344a31ea3725df7fb
christinab980 <71195516+christinab980@users.noreply.github.com>
2023-05-27 09:45:20 -0700

    updated main.jsx with views of pages

--- my-react-project/src/sectioning/main.jsx
+++ my-react-project/src/sectioning/main.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Cateogory, Landing, Quiz } from '../views';
 
 const Main = () => {
   return (
-    <main>
-      <div className="y-wrap">Main</div>
+    <main className="y-wrap">
+      <Routes>
+        <Route path="/category" element={<Cateogory />} />
+        <Route path="/" element={<Landing />} />
+        <Route path="/quiz" element={<Quiz />} />
+      </Routes>
     </main>
   )
 };
