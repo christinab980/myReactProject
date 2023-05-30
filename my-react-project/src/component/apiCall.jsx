d4aa663e14ce056963d5a89aebc17196da7743ef
christinab980 <71195516+christinab980@users.noreply.github.com>
2023-05-30 16:44:16 -0700

    added api, fetched and styled data

--- my-react-project/src/component/apiCall.jsx
+++ my-react-project/src/component/apiCall.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 // API URLS with specific categories
 
 //https://quizapi.io/api/v1/questions?apiKey=YOUR_API_KEY&category=code&difficulty=Easy&limit=10&tags=HTML
@@ -11,3 +13,14 @@
 // -d apiKey="w9Rvsy8CdKGevTtBTBwe0aGMiqhMO7sHiJx57y8Y" 
 // -d category=Programming
 // -d limit=10
+
+const apiKey = "w9Rvsy8CdKGevTtBTBwe0aGMiqhMO7sHiJx57y8Y"
+
+async function fetchCallforHTML() {
+    const url = `https://quizapi.io/api/v1/questions?apiKey=${apiKey}&category=code&difficulty=Easy&limit=10&tags=HTML`
+    const response = await fetch(url);
+    const data = await response.json()
+    console.log(data)
+}
+
+fetchCallforHTML()
--- my-react-project/src/views/quiz.jsx
+++ my-react-project/src/views/quiz.jsx
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Quiz = () => {
+  const [quizOptions, setQuiz] = useState([]);
+
+  const apiKey = "w9Rvsy8CdKGevTtBTBwe0aGMiqhMO7sHiJx57y8Y";
+  const tags = ['html', 'JavaScript', 'MySQL']
+
+  useEffect(() => {
+    fetch( `https://quizapi.io/api/v1/questions?apiKey=${apiKey}&category=code&difficulty=Easy&limit=10&tags=HTML`)
+    .then(response => response.json())
+    .then(data => setQuiz(data))
+
+  }, [])
+
+  console.log(quizOptions)
+
   return (
-    <h1>Quiz</h1>
+    <div className='quiz-container'>
+    <h2>Quiz</h2>
+    <div className='html-quiz'>
+      {quizOptions.map(quiz => (
+        <div key={quiz.id}>
+          <div className='quiz-question'>{quiz.question}</div>
+          <div className='quiz-answers'> 
+            <div className='quiz-answer'>{quiz.answers.answer_a}</div>
+            <div className='quiz-answer'>{quiz.answers.answer_b}</div>
+            <div className='quiz-answer'>{quiz.answers.answer_c}</div>
+            <div className='quiz-answer'>{quiz.answers.answer_d}</div>
+          </div>
+        </div>
+      ))}
+    </div>
+  </div>
   )
 };
 
